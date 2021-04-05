67ac3c708369fc69033e4373e0a8495d83ffb886
Vlad70003 <porohin1995@gmail.com>
2021-04-05 21:53:32 +0300

    final homework 1 after edit 05/04/21:53

--- projects/final_project-1/script.js
+++ projects/final_project-1/script.js
@@ -1,5 +1,8 @@
 ymaps.ready(init);
 
+let map = document.querySelector('#map');
+let placemarks = [];
+
 function init() {
   var myPlacemark,
     myMap = new ymaps.Map('map', {
@@ -7,68 +10,98 @@ function init() {
       zoom: 12
     }, {
       searchControlProvider: 'yandex#search'
-    });
-
-
+    },
+    clusterer = new ymaps.Clusterer({
+      groupByCoordinates: true,
+      clusterDisableClickZoom: true,
+      clusterOpenBalloonOnClick: false,
+    })
+    );
+    
+  ///Получаем доступ к форме
+	let form = document.querySelector('#addForm').innerHTML;
+  
+  
   // Слушаем клик на карте.
   myMap.events.add('click', function(e) {
     let coords = e.get('coords');
-    let target = e.target;
-    let form = document.querySelector('#addForm').innerHTML;
-
-    newBaloon = openBaloon(coords, form);
-
-    myNewMark = createPlacemark(coords, createForm('click', form, document.body)());
+       
+    let baloon = new Baloon();
     
-
-    myMap.geoObjects.add(myNewMark, coords);
+    baloon.openBaloon(coords);
+    document.body.addEventListener('click', event => {
+      let target = event.target;
+      
+      if(target.dataset.role == 'review-add'){
+        
+        let reviewForm = document.querySelector('[data-role=review-form]');
+        let coords = JSON.parse(reviewForm.dataset.coords);
+        placemarks.push({
+          coords,
+          review: {
+            name: document.querySelector('[data-role=review-name]').value,
+            place: document.querySelector('[data-role=review-place]').value,
+            text: document.querySelector('[data-role=review-text]').value,
+          },
+        });
+        baloon.createPlacemark(coords);
+      }
+    })
+    
   });
 
-  // Создание метки.
-  function createPlacemark(coords, baloon) {
   
+
+
+  function Baloon() {
   
+  	this.openBaloon = function(coords) {
+    	let newForm = this.createForm(coords, placemarks);
+      myMap.balloon.open(coords, newForm.innerHTML, {
+        closeButton: true
+      });
+    };
+  	
   
-    let placemark = new ymaps.Placemark(coords, {
-      balloonContentBody: baloon
-    });
-
-    return placemark;
-  }
-
-  // Создание балуна
-  function openBaloon(coords, content) {
-
-    myMap.balloon.open(coords, content, {
-      closeButton: true
-    });
-
-  }
-}
-
-  
-function createForm(event, form, target){
-return function(){
-	target.addEventListener(event, e => {
-  	let targ = e.target;
-  	if(targ.tagName == 'BUTTON'){
-    	let nameValue = document.querySelector('#input-name').value;
-      let placeValue = document.querySelector('#input-place').value;
-      let commentValue = document.querySelector('#input-comm').value;
-      let review = document.querySelector('#review');
-      let personInf = document.createElement('div');
-      let comment = document.createElement('div');
-      let form = document.querySelector('#addForm').innerHTML;
-      personInf.textContent = `${nameValue} ${placeValue}`
-      comment.textContent = commentValue;
-
-      review.append(personInf);
-      review.append(comment);
-      review.append(form);
-    
-    return review;
+  	this.createForm = function(coords, placemarks){
+      let similar = false;
+      for(let element of placemarks){
+        if(element.coords[0] == coords[0] && element.coords[1] == coords[1]){
+          similar = true;
+        }
+      }
+      console.log(similar)
+    	let newForm = document.createElement('div');
+      newForm.innerHTML = form;
+      let reviewList = newForm.querySelector('#review-list');
+      let reviewForm = newForm.querySelector('[data-role=review-form]');
+      reviewForm.dataset.coords = JSON.stringify(coords);
+      if (similar == true){
+        for (let item of placemarks){
+          let div = document.createElement('div');
+          div.classList.add('review-item');
+          div.innerHTML = `
+          <div>
+            <b>${item.name}</b> [${item.place}]
+          </div>
+          <div>${item.text}</div>
+          `;
+          reviewList.append(div);
+        }
+      }
+      return newForm;
     }
-    
+
+   	this.createPlacemark = function(coords) {
+      let placemark = new ymaps.Placemark(coords);
+    	placemark.events.add('click', event => {
+      	let coords = event.get('coords');
+        this.openBaloon(coords)
+      })
+      myMap.geoObjects.add(placemark) 
+    }
+    this.closeBaloon = function(){
+
+    }
   }
-)}
 }
