d7f5d0e318e78d185206c4df0e4d09ddfaf68426
Vlad70003 <porohin1995@gmail.com>
2021-04-06 22:26:33 +0300

    complate final homework 1

--- projects/final_project-1/script.js
+++ projects/final_project-1/script.js
@@ -7,17 +7,29 @@ let geoObjects = [];
 let placemarks = [];
 
 function init() {
-  var myPlacemark,
-    myMap = new ymaps.Map('map', {
-      center: [55.753994, 37.622093],
-      zoom: 12
-    }, {
-      searchControlProvider: 'yandex#search'
-    });
-    
-   
-    
-    ///Доступ к конструктору
+  let myMap = new ymaps.Map('map', {
+    center: [55.753994, 37.622093],
+    zoom: 12
+  }, {
+    searchControlProvider: 'yandex#search'
+  });
+
+  let clusterer = new ymaps.Clusterer({
+    preset: 'islands#invertedVioletClusterIcons',
+    groupByCoordinates: true,
+    clusterDisableClickZoom: true,
+    clusterOpenBalloonOnClick: false,
+  });
+
+  clusterer.events.add('click', event => {
+    let coords = event.get('target').geometry.getCoordinates();
+    baloon.openBaloon(coords)
+  })
+
+  myMap.geoObjects.add(clusterer);
+
+
+  ///Доступ к конструктору
   let baloon = new Baloon();
 
   ///Получаем доступ к форме
@@ -25,38 +37,37 @@ function init() {
 
 
   // Слушаем клик на карте.
-  myMap.events.add('click', function(e) {
+  myMap.events.add('click', function (e) {
     let coords = e.get('coords');
 
 
     baloon.openBaloon(coords);
 
-    document.body.addEventListener('click', event => {
-      let target = event.target;
-      if (target.dataset.role == 'review-add') {
-        let reviewForm = document.querySelector('[data-role=review-form]');
-        let coords = JSON.parse(reviewForm.dataset.coords);
-        placemarks.push({
-          coords,
-          review: {
-            name: document.querySelector('[data-role=review-name]').value,
-            place: document.querySelector('[data-role=review-place]').value,
-            text: document.querySelector('[data-role=review-text]').value,
-          },
-        });
-       baloon.closeBaloon();
-       baloon.createClasterer();
-       baloon.createPlacemark(coords);
-       
-      }
-    })
- 		    
   });
 
-  function Baloon() {
-  	
+  document.body.addEventListener('click', event => {
+    let target = event.target;
+    if (target.dataset.role == 'review-add') {
+      let reviewForm = document.querySelector('[data-role=review-form]');
+      let coords = JSON.parse(reviewForm.dataset.coords);
+      placemarks.push({
+        coords,
+        review: {
+          name: document.querySelector('[data-role=review-name]').value,
+          place: document.querySelector('[data-role=review-place]').value,
+          text: document.querySelector('[data-role=review-text]').value,
+        },
+      });
+      baloon.closeBaloon();
+      baloon.createPlacemark(coords);
 
-    this.openBaloon = function(coords) {
+    }
+  })
+
+  function Baloon() {
+
+
+    this.openBaloon = function (coords) {
       let newForm = this.createForm(coords, placemarks);
       this.setBaloonContent(newForm.innerHTML);
       myMap.balloon.open(coords, newForm.innerHTML, {
@@ -65,7 +76,7 @@ function init() {
     };
 
 
-    this.createForm = function(coords, placemarks) {
+    this.createForm = function (coords, placemarks) {
 
       let newForm = document.createElement('div');
       newForm.innerHTML = form;
@@ -74,7 +85,7 @@ function init() {
       reviewForm.dataset.coords = JSON.stringify(coords);
 
       for (let i = 0; i < placemarks.length; i++) {
-      geoObjects[i] = new ymaps.Placemark(placemarks[i].coords);
+        geoObjects[i] = new ymaps.Placemark(placemarks[i].coords);
         if (placemarks[i].coords[0] == coords[0] && placemarks[i].coords[1] == coords[1]) {
           let div = document.createElement('div');
           div.classList.add('review-item');
@@ -90,38 +101,21 @@ function init() {
       return newForm;
     }
 
-    this.createPlacemark = function(coords) {
+    this.createPlacemark = function (coords) {
       let placemark = new ymaps.Placemark(coords);
       placemark.events.add('click', event => {
         let coords = event.get('target').geometry.getCoordinates();
         this.openBaloon(coords)
       })
       myMap.geoObjects.add(placemark);
-      
+      clusterer.add(placemark);
     }
 
-    this.createClasterer = function(placemarks) {
-     let clusterer = new ymaps.Clusterer({
-       	preset: 'islands#invertedVioletClusterIcons',
-        groupByCoordinates: true,
-        clusterDisableClickZoom: true,
-            clusterHideIconOnBalloonOpen: false,
-            geoObjectHideIconOnBalloonOpen: false
-      });
-      clusterer.events.add('click', event => {
-        let coords = event.get('target').geometry.getCoordinates();
-        this.openBaloon(coords)
-      })
-      clusterer.add(geoObjects);
-      myMap.geoObjects.add(clusterer);
-     
-    }
-
-    this.closeBaloon = function() {
+    this.closeBaloon = function () {
       myMap.balloon.close()
     }
 
-    this.setBaloonContent = function(content) {
+    this.setBaloonContent = function (content) {
       myMap.balloon.setData(content);
     }
 
