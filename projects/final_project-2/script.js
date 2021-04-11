b75bcfb91fd61ee87c26c972a48ac8a783d86a17
Vlad70003 <porohin1995@gmail.com>
2021-04-11 20:25:00 +0300

    final_project-2 after edit 11/04/20:24

--- projects/final_project-2/script.js
+++ projects/final_project-2/script.js
@@ -3,7 +3,8 @@ let users = [];
 let name = document.querySelector('#name');
 let nickname = document.querySelector("#nickname");
 let button = document.querySelector("#button-autorization");
-
+let chatList = document.querySelector(".left-side__list");
+let chatAction = new Chat();
 
 document.addEventListener('click', event => {
     let target = event.target;
@@ -13,8 +14,7 @@ document.addEventListener('click', event => {
         try{if(name.value.length > 0 && nickname.value.length > 0){
             users.push({name: name.value, nickname:nickname.value});
             let windowAutorization = target.closest('.visible');
-
-            
+            chatAction.addChatPerson(name.value, nickname.value);
 
             if(windowAutorization.classList){
                 windowAutorization.classList.toggle("hide");
@@ -26,10 +26,41 @@ document.addEventListener('click', event => {
             target.nextElementSibling.textContent = e.message;
         }
     }
+
+    
 })
 
-function chatAction(){
-    this.addChat = function(){
-        
+function Chat(){
+
+    this.addChatPerson = function(name, nickname){
+        let newPerson = this.createPerson(name, nickname);
+        chatList.append(newPerson);
+    }
+
+    this.createPerson = function(name, nickname){
+        let person = document.createElement('li');
+        person.classList.add('left-side__item');
+
+        let avatar = document.createElement('span');
+        avatar.classList.add('avatar');
+
+        let personInformation = document.createElement("div");
+        personInformation.classList.add("person-information");
+
+        let nameValue = document.createElement("div");
+        nameValue.classList.add('person-information__name');
+        nameValue.textContent = name;
+
+        let nickNameValue = document.createElement("div");
+        nickNameValue.classList.add('person-information__nikname');
+        nickNameValue.textContent = nickname;
+
+        personInformation.append(nameValue);
+        personInformation.append(nickNameValue);
+
+        person.append(avatar);
+        person.append(personInformation);
+
+        return person;
     }
 }
